33b6b4988b64819fe93b6428b3f9cb3aa280b054
Stephen Gold <sgold@sonic.net>
2019-04-02 22:47:33 -0700

    btSimpleBroadphase: final fix for JME issue 1058

--- src/native/bullet3/BulletCollision/BroadphaseCollision/btSimpleBroadphase.cpp
+++ src/native/bullet3/BulletCollision/BroadphaseCollision/btSimpleBroadphase.cpp
@@ -123,9 +123,11 @@ protected:
 
 void btSimpleBroadphase::destroyProxy(btBroadphaseProxy* proxyOrg, btDispatcher* dispatcher)
 {
-	btSimpleBroadphaseProxy* proxy0 = static_cast<btSimpleBroadphaseProxy*>(proxyOrg);
 	m_pairCache->removeOverlappingPairsContainingProxy(proxyOrg, dispatcher);
-	freeHandle(proxy0); // sgold swapped this stmt with the preceeding one
+
+	btSimpleBroadphaseProxy* proxy0 = static_cast<btSimpleBroadphaseProxy*>(proxyOrg);
+	freeHandle(proxy0);
+
 	//validate();
 }
 
