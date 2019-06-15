4e9cdc0d063313a79e391f345456bd0ddad39c31
Stephen Gold <sgold@sonic.net>
2019-06-15 13:20:55 -0700

    rewrite the native mesh interface: CompoundMesh and IndexedMesh

---
+++ src/native/glue/com_jme3_bullet_collision_shapes_infos_CompoundMesh.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2019 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "com_jme3_bullet_collision_shapes_infos_CompoundMesh.h"
+#include "jmeBulletUtil.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_CompoundMesh
+     * Method:    addIndexedMesh
+     * Signature: (JJ)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_collision_shapes_infos_CompoundMesh_addIndexedMesh
+    (JNIEnv *env, jobject object, jlong compoundMeshId, jlong submeshId) {
+        btTriangleIndexVertexArray *pMesh
+                = reinterpret_cast<btTriangleIndexVertexArray *> (compoundMeshId);
+        NULL_CHECK(pMesh, "The btTriangleIndexVertexArray does not exist.",);
+
+        btIndexedMesh *pSubmesh = reinterpret_cast<btIndexedMesh *> (submeshId);
+        NULL_CHECK(pSubmesh, "The btIndexedMesh does not exist.",);
+
+        pMesh->addIndexedMesh(*pSubmesh);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_CompoundMesh
+     * Method:    createEmptyTiva
+     * Signature: ()J
+     */
+    JNIEXPORT jlong JNICALL Java_com_jme3_bullet_collision_shapes_infos_CompoundMesh_createEmptyTiva
+    (JNIEnv *env, jobject object) {
+        jmeClasses::initJavaClasses(env);
+        btTriangleIndexVertexArray *pMesh = new btTriangleIndexVertexArray();
+        return reinterpret_cast<jlong> (pMesh);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_CompoundMesh
+     * Method:    finalizeNative
+     * Signature: (J)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_collision_shapes_infos_CompoundMesh_finalizeNative
+    (JNIEnv *env, jobject object, jlong meshId) {
+        btTriangleIndexVertexArray* pMesh
+                = reinterpret_cast<btTriangleIndexVertexArray *> (meshId);
+        NULL_CHECK(pMesh, "The btTriangleIndexVertexArray does not exist.",);
+
+        delete pMesh;
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_CompoundMesh
+     * Method:    getScaling
+     * Signature: (JLcom/jme3/math/Vector3f;)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_collision_shapes_infos_CompoundMesh_getScaling
+    (JNIEnv *env, jobject object, jlong meshId, jobject storeVector) {
+        btTriangleIndexVertexArray* pMesh
+                = reinterpret_cast<btTriangleIndexVertexArray *> (meshId);
+        NULL_CHECK(pMesh, "The btTriangleIndexVertexArray does not exist.",);
+        NULL_CHECK(storeVector, "The store vector does not exist.",);
+
+        const btVector3 *pScale = &pMesh->getScaling();
+        jmeBulletUtil::convert(env, pScale, storeVector);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_CompoundMesh
+     * Method:    setScaling
+     * Signature: (JFFF)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_collision_shapes_infos_CompoundMesh_setScaling
+    (JNIEnv *env, jobject object, jlong meshId, jfloat xScale, jfloat yScale,
+            jfloat zScale) {
+        btTriangleIndexVertexArray* pMesh
+                = reinterpret_cast<btTriangleIndexVertexArray *> (meshId);
+        NULL_CHECK(pMesh, "The btTriangleIndexVertexArray does not exist.",);
+
+        btVector3 scale;
+        scale.setX(xScale);
+        scale.setY(yScale);
+        scale.setZ(zScale);
+        pMesh->setScaling(scale);
+    }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/native/glue/com_jme3_bullet_collision_shapes_infos_IndexedMesh.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2019 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "com_jme3_bullet_collision_shapes_infos_IndexedMesh.h"
+#include "jmeBulletUtil.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_IndexedMesh
+     * Method:    createByte
+     * Signature: (Ljava/nio/ByteBuffer;Ljava/nio/FloatBuffer;IIII)J
+     */
+    JNIEXPORT jlong JNICALL Java_com_jme3_bullet_collision_shapes_infos_IndexedMesh_createByte
+    (JNIEnv *env, jobject object, jobject byteBuffer, jobject floatBuffer,
+            jint numTriangles, jint numVertices, jint vertexStride,
+            jint indexStride) {
+        jmeClasses::initJavaClasses(env);
+
+        NULL_CHECK(byteBuffer, "The index buffer does not exist.", 0);
+        unsigned char *pIndices
+                = (unsigned char *) env->GetDirectBufferAddress(byteBuffer);
+
+        NULL_CHECK(floatBuffer, "The position buffer does not exist.", 0);
+        unsigned char *pVertices
+                = (unsigned char *) env->GetDirectBufferAddress(floatBuffer);
+
+        btIndexedMesh *pMesh = new btIndexedMesh();
+        pMesh->m_indexType = PHY_UCHAR;
+        pMesh->m_triangleIndexBase = pIndices;
+        pMesh->m_vertexBase = pVertices;
+        pMesh->m_numTriangles = numTriangles;
+        pMesh->m_numVertices = numVertices;
+        pMesh->m_vertexStride = vertexStride;
+        pMesh->m_triangleIndexStride = indexStride;
+
+        return reinterpret_cast<jlong> (pMesh);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_IndexedMesh
+     * Method:    createInt
+     * Signature: (Ljava/nio/IntBuffer;Ljava/nio/FloatBuffer;IIII)J
+     */
+    JNIEXPORT jlong JNICALL Java_com_jme3_bullet_collision_shapes_infos_IndexedMesh_createInt
+    (JNIEnv *env, jobject object, jobject intBuffer, jobject floatBuffer,
+            jint numTriangles, jint numVertices, jint vertexStride,
+            jint indexStride) {
+        jmeClasses::initJavaClasses(env);
+
+        NULL_CHECK(intBuffer, "The index buffer does not exist.", 0);
+        unsigned char *pIndices
+                = (unsigned char *) env->GetDirectBufferAddress(intBuffer);
+
+        NULL_CHECK(floatBuffer, "The position buffer does not exist.", 0);
+        unsigned char *pVertices
+                = (unsigned char *) env->GetDirectBufferAddress(floatBuffer);
+
+        btIndexedMesh *pMesh = new btIndexedMesh();
+        pMesh->m_indexType = PHY_INTEGER;
+        pMesh->m_triangleIndexBase = pIndices;
+        pMesh->m_vertexBase = pVertices;
+        pMesh->m_numTriangles = numTriangles;
+        pMesh->m_numVertices = numVertices;
+        pMesh->m_vertexStride = vertexStride;
+        pMesh->m_triangleIndexStride = indexStride;
+
+        return reinterpret_cast<jlong> (pMesh);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_IndexedMesh
+     * Method:    createShort
+     * Signature: (Ljava/nio/ShortBuffer;Ljava/nio/FloatBuffer;IIII)J
+     */
+    JNIEXPORT jlong JNICALL Java_com_jme3_bullet_collision_shapes_infos_IndexedMesh_createShort
+    (JNIEnv *env, jobject object, jobject shortBuffer, jobject floatBuffer,
+            jint numTriangles, jint numVertices, jint vertexStride,
+            jint indexStride) {
+        jmeClasses::initJavaClasses(env);
+
+        NULL_CHECK(shortBuffer, "The index buffer does not exist.", 0);
+        unsigned char *pIndices
+                = (unsigned char *) env->GetDirectBufferAddress(shortBuffer);
+
+        NULL_CHECK(floatBuffer, "The position buffer does not exist.", 0);
+        unsigned char *pVertices
+                = (unsigned char *) env->GetDirectBufferAddress(floatBuffer);
+
+        btIndexedMesh *pMesh = new btIndexedMesh();
+        pMesh->m_indexType = PHY_SHORT;
+        pMesh->m_triangleIndexBase = pIndices;
+        pMesh->m_vertexBase = pVertices;
+        pMesh->m_numTriangles = numTriangles;
+        pMesh->m_numVertices = numVertices;
+        pMesh->m_vertexStride = vertexStride;
+        pMesh->m_triangleIndexStride = indexStride;
+
+        return reinterpret_cast<jlong> (pMesh);
+    }
+
+    /*
+     * Class:     com_jme3_bullet_collision_shapes_infos_IndexedMesh
+     * Method:    finalizeNative
+     * Signature: (J)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_collision_shapes_infos_IndexedMesh_finalizeNative
+    (JNIEnv *env, jobject object, jlong meshId) {
+        btIndexedMesh *pMesh = reinterpret_cast<btIndexedMesh *> (meshId);
+        NULL_CHECK(pMesh, "The btIndexedMesh does not exist.",);
+
+        delete pMesh;
+    }
+
+#ifdef __cplusplus
+}
+#endif
