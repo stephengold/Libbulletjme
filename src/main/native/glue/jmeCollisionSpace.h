d8ddafc2e3700ac5ebb3c3ba2f8691eb925d24f9
Stephen Gold <sgold@sonic.net>
2020-03-10 17:16:01 -0700

    jmeCollisionSpace.h: guard against multiple inclusions

--- src/main/native/glue/jmeCollisionSpace.h
+++ src/main/native/glue/jmeCollisionSpace.h
@@ -29,6 +29,9 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef _Included_jmeCollisionSpace
+#define _Included_jmeCollisionSpace
+
 #include <jni.h>
 #include "btBulletCollisionCommon.h"
 #include "BulletCollision/CollisionDispatch/btCollisionObject.h"
@@ -89,3 +92,5 @@ public:
         return m_javaSpace;
     }
 };
+
+#endif
