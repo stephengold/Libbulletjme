302f20439c8c9d79903eda08a8670212962411a0
Stephen Gold <sgold@sonic.net>
2020-03-24 01:15:28 -0700

    PhysicsCollisionObject: add boundingBox() method

--- src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
+++ src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
@@ -31,6 +31,7 @@
  */
 package com.jme3.bullet.collision;
 
+import com.jme3.bounding.BoundingBox;
 import com.jme3.bullet.CollisionSpace;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.math.Matrix3f;
@@ -180,6 +181,27 @@ abstract public class PhysicsCollisionObject
         if (objectId != 0L) {
             setCollideWithGroups(objectId, collideWithGroups);
         }
+    }
+
+    /**
+     * Calculate an axis-aligned bounding box for this object, based on its
+     * collision shape. Note: the calculated bounds are seldom minimal; they are
+     * typically larger than necessary due to centering constraints and
+     * collision margins.
+     *
+     * @param storeResult storage for the result (modified if not null)
+     * @return a bounding box in physics-space coordinates (either storeResult
+     * or a new instance)
+     */
+    public BoundingBox boundingBox(BoundingBox storeResult) {
+        BoundingBox result
+                = (storeResult == null) ? new BoundingBox() : storeResult;
+
+        Vector3f translation = getPhysicsLocation(null);
+        Matrix3f rotation = getPhysicsRotationMatrix(null);
+        collisionShape.boundingBox(translation, rotation, result);
+
+        return result;
     }
 
     /**
