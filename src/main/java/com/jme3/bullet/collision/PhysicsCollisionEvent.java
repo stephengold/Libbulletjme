fbb2db0c3567fca68a195e5d34837af4e93c2af9
Stephen Gold <sgold@sonic.net>
2020-04-30 23:26:49 -0700

    comments

--- src/main/java/com/jme3/bullet/collision/PhysicsCollisionEvent.java
+++ src/main/java/com/jme3/bullet/collision/PhysicsCollisionEvent.java
@@ -40,7 +40,7 @@ import jme3utilities.Validate;
  * Describe a collision between 2 collision objects in a PhysicsSpace.
  * <p>
  * Even though this class inherits the java.io.Serializable interface, it isn't
- * serializable.
+ * serializable. TODO extend PhysicsNativeObject
  *
  * @author normenhansen
  */
