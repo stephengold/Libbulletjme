0f0f6ee740026aa692cd3df3fb5d4ab8ab708f39
Stephen Gold <sgold@sonic.net>
2020-05-24 14:07:39 -0700

    convert tabs to spaces in 2 header files

--- src/main/native/glue/jmeBulletUtil.h
+++ src/main/native/glue/jmeBulletUtil.h
@@ -38,33 +38,33 @@
  */
 class jmeBulletUtil {
 public:
-    // convert Bullet math objects to jMonkeyEngine:
-    static void convert(JNIEnv *, const btMatrix3x3 * in, jobject outMatrix);
-    static void convert(JNIEnv *, const btQuaternion * in, jobject outQuat);
-    static void convert(JNIEnv *, const btTransform * in, jobject outTransform);
-    static void convert(JNIEnv *, const btVector3 * in, jobject outVector);
-    static void convertQuat(JNIEnv *, const btMatrix3x3 * in, jobject outQuat);
+	// convert Bullet math objects to jMonkeyEngine:
+	static void convert(JNIEnv *, const btMatrix3x3 * in, jobject outMatrix);
+	static void convert(JNIEnv *, const btQuaternion * in, jobject outQuat);
+	static void convert(JNIEnv *, const btTransform * in, jobject outTransform);
+	static void convert(JNIEnv *, const btVector3 * in, jobject outVector);
+	static void convertQuat(JNIEnv *, const btMatrix3x3 * in, jobject outQuat);
 
-    // convert jMonkeyEngine math objects to Bullet:
-    static void convert(JNIEnv *, jobject inMatrix, btMatrix3x3 * out);
-    static void convert(JNIEnv *, jobject inQuat, btQuaternion * out);
-    static void convert(JNIEnv *, jobject inTransform,
-            btTransform * outTransform, btVector3 * outScale);
-    static void convert(JNIEnv *, jobject inVector, btVector3 * out);
-    static void convertQuat(JNIEnv *, jobject inQuat, btMatrix3x3 * out);
+	// convert jMonkeyEngine math objects to Bullet:
+	static void convert(JNIEnv *, jobject inMatrix, btMatrix3x3 * out);
+	static void convert(JNIEnv *, jobject inQuat, btQuaternion * out);
+	static void convert(JNIEnv *, jobject inTransform,
+			btTransform * outTransform, btVector3 * outScale);
+	static void convert(JNIEnv *, jobject inVector, btVector3 * out);
+	static void convertQuat(JNIEnv *, jobject inQuat, btMatrix3x3 * out);
 
-    static void addRayTestResult(JNIEnv *, jobject resultList,
-            const btVector3 *pNormal, btScalar hitFraction,
-            const btCollisionObject *, int partIndex, int triangleIndex);
-    static void addSweepTestResult(JNIEnv *, jobject resultList,
-            const btVector3 *pNormal, btScalar hitFraction,
-            const btCollisionObject *, int partIndex, int triangleIndex);
+	static void addRayTestResult(JNIEnv *, jobject resultList,
+			const btVector3 *pNormal, btScalar hitFraction,
+			const btCollisionObject *, int partIndex, int triangleIndex);
+	static void addSweepTestResult(JNIEnv *, jobject resultList,
+			const btVector3 *pNormal, btScalar hitFraction,
+			const btCollisionObject *, int partIndex, int triangleIndex);
 
 private:
 
-    jmeBulletUtil() {
-    };
+	jmeBulletUtil() {
+	};
 
-    ~jmeBulletUtil() {
-    };
+	~jmeBulletUtil() {
+	};
 };
