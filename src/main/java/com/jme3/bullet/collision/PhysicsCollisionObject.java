cd7c11d69265c7319944a5882496369b490e56e9
Stephen Gold <sgold@sonic.net>
2020-08-07 18:39:17 -0700

    PhysicsCollisionObject: add the clearIgnoreList() method

--- src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
+++ src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
@@ -220,6 +220,18 @@ abstract public class PhysicsCollisionObject
     }
 
     /**
+     * Clear this object's ignore list.
+     */
+    public void clearIgnoreList() {
+        long thisId = nativeId();
+        long[] otherIds = listIgnoredIds();
+        for (long otherId : otherIds) {
+            boolean toIgnore = false;
+            setIgnoreCollisionCheck(thisId, otherId, toIgnore);
+        }
+    }
+
+    /**
      * Copy common properties from another PhysicsCollisionObject.
      *
      * @param old (not null, unaffected)
