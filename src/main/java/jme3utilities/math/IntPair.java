c5c1824581db5f22ea64157f3eb703a1a2a3c8bd
Stephen Gold <sgold@sonic.net>
2021-01-22 19:04:20 -0800

    IntPair: avoid accessing private fields of another instance

--- src/main/java/jme3utilities/math/IntPair.java
+++ src/main/java/jme3utilities/math/IntPair.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Stephen Gold
+ * Copyright (c) 2020-2021, Stephen Gold
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -116,8 +116,8 @@ public class IntPair {
         } else if (otherObject != null
                 && otherObject.getClass() == getClass()) {
             IntPair otherEdge = (IntPair) otherObject;
-            result = (otherEdge.larger == larger)
-                    && (otherEdge.smaller == smaller);
+            result = (otherEdge.larger() == larger)
+                    && (otherEdge.smaller() == smaller);
         } else {
             result = false;
         }
@@ -126,7 +126,7 @@ public class IntPair {
     }
 
     /**
-     * Generate the hash code for this IntPair.
+     * Generate the hash code for this pair.
      *
      * @return the value to use for hashing
      */
