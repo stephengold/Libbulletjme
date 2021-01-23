3f05b60178b8fc5e8ff92cb117cf2bcce8a91e0b
Stephen Gold <sgold@sonic.net>
2021-01-22 19:04:49 -0800

    publicize 2 loggers

--- src/main/java/jme3utilities/math/MyBuffer.java
+++ src/main/java/jme3utilities/math/MyBuffer.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2019-2020, Stephen Gold
+ Copyright (c) 2019-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -52,7 +52,7 @@ final public class MyBuffer {
     /**
      * message logger for this class
      */
-    final private static Logger logger
+    final public static Logger logger
             = Logger.getLogger(MyBuffer.class.getName());
     // *************************************************************************
     // constructors
--- src/main/java/jme3utilities/math/MyMath.java
+++ src/main/java/jme3utilities/math/MyMath.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013-2020, Stephen Gold
+ Copyright (c) 2013-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -40,7 +40,7 @@ public class MyMath {
     /**
      * message logger for this class
      */
-    final private static Logger logger
+    final public static Logger logger
             = Logger.getLogger(MyMath.class.getName());
     // *************************************************************************
     // constructors
