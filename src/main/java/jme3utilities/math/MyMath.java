19159d260ce5ed8e222b5586216b0008bb839047
Stephen Gold <sgold@sonic.net>
2022-05-09 14:26:55 -0700

    MyMath:  add the "rootHalf" constant

--- src/main/java/jme3utilities/math/MyMath.java
+++ src/main/java/jme3utilities/math/MyMath.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013-2021, Stephen Gold
+ Copyright (c) 2013-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -26,6 +26,7 @@
  */
 package jme3utilities.math;
 
+import com.jme3.math.FastMath;
 import java.util.logging.Logger;
 
 /**
@@ -37,6 +38,10 @@ public class MyMath {
     // *************************************************************************
     // constants and loggers
 
+    /**
+     * square root of 1/2
+     */
+    final public static float rootHalf = FastMath.sqrt(0.5f);
     /**
      * message logger for this class
      */
