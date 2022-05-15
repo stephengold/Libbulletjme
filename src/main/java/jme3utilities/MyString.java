61a2308c044d487a11c4582db1839ec3b949e442
Stephen Gold <sgold@sonic.net>
2022-05-14 22:16:53 -0700

    MyString:  add the escape() and quote() methods

--- src/main/java/jme3utilities/MyString.java
+++ src/main/java/jme3utilities/MyString.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013-2021, Stephen Gold
+ Copyright (c) 2013-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -55,10 +55,49 @@ public class MyString {
     // new methods exposed
 
     /**
-     * Convert the first character of the specified String to lower case.
+     * Replace all tab, quote, newline, and backslash characters in the
+     * specified text with escape sequences.
      *
-     * @param input the input string (not null)
-     * @return the converted String (not null)
+     * @param unescaped the input text to escape (not null)
+     * @return the escaped text (not null)
+     */
+    public static String escape(CharSequence unescaped) {
+        int length = unescaped.length();
+        StringBuilder result = new StringBuilder(length + 10);
+
+        for (int i = 0; i < length; ++i) {
+            char ch = unescaped.charAt(i);
+            switch (ch) {
+                case '\n':
+                    result.append("\\n");
+                    break;
+
+                case '\t':
+                    result.append("\\t");
+                    break;
+
+                case '"':
+                    result.append("\\\"");
+                    break;
+
+                case '\\':
+                    result.append("\\\\");
+                    break;
+
+                default:
+                    result.append(ch);
+                    break;
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Convert the first character of the specified text to lower case.
+     *
+     * @param input the input text to convert (not null)
+     * @return the converted text (not null)
      */
     public static String firstToLower(String input) {
         String result = input;
@@ -67,6 +106,24 @@ public class MyString {
             first = first.toLowerCase(Locale.ROOT);
             String rest = input.substring(1);
             result = first + rest;
+        }
+
+        return result;
+    }
+
+    /**
+     * Enclose the specified text in quotation marks and escape all tab, quote,
+     * newline, and backslash characters.
+     *
+     * @param text the input text to quote
+     * @return the quoted text, or "null" if the input was null
+     */
+    public static String quote(CharSequence text) {
+        String result;
+        if (text == null) {
+            result = "null";
+        } else {
+            result = "\"" + escape(text) + "\"";
         }
 
         return result;
