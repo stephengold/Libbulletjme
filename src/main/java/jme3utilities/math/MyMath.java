d63404bf9ca23c8af61f01c02282f67d92604674
Stephen Gold <sgold@sonic.net>
2022-06-03 17:44:22 -0700

    MyMath:  add the "phi" constant

--- src/main/java/jme3utilities/math/MyMath.java
+++ src/main/java/jme3utilities/math/MyMath.java
@@ -43,6 +43,10 @@ public class MyMath {
     // constants and loggers
 
     /**
+     * golden ratio = 1.618...
+     */
+    final public static float phi = (1f + FastMath.sqrt(5f)) / 2f;
+    /**
      * square root of 1/2
      */
     final public static float rootHalf = FastMath.sqrt(0.5f);
