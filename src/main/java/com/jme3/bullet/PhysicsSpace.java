5b2c1ce62d6845dd054995dd0486de35f85ffefd
Stephen Gold <sgold@sonic.net>
2022-06-30 13:52:07 -0700

    PhysicsSpace:  comment

--- src/main/java/com/jme3/bullet/PhysicsSpace.java
+++ src/main/java/com/jme3/bullet/PhysicsSpace.java
@@ -1142,11 +1142,12 @@ public class PhysicsSpace
         }
         long rigidBodyId = rigidBody.nativeId();
         rigidMap.put(rigidBodyId, rigidBody);
-
-        //Workaround
-        //It seems that adding a Kinematic RigidBody to the dynamicWorld
-        //prevents it from being dynamic again afterward.
-        //So we add it dynamic, then set it kinematic.
+        /*
+         * Workaround:
+         * It seems that adding a Kinematic RigidBody to the dynamicWorld
+         * prevents it from being dynamic again afterward.
+         * So we add it dynamic, then set it kinematic.
+         */
         boolean kinematic = false;
         if (rigidBody.isKinematic()) {
             kinematic = true;
