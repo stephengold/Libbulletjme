e18ccf55adcc4c27c1694a17d7caaf5bfd7d8277
Stephen Gold <sgold@sonic.net>
2022-08-06 20:37:43 -0700

    PhysicsSoftSpace:  add a simplified constructor

--- src/main/java/com/jme3/bullet/PhysicsSoftSpace.java
+++ src/main/java/com/jme3/bullet/PhysicsSoftSpace.java
@@ -76,6 +76,17 @@ public class PhysicsSoftSpace extends PhysicsSpace {
      * Instantiate a PhysicsSoftSpace with a sequential-impulse solver. Must be
      * invoked on the designated physics thread.
      *
+     * @param broadphaseType which broadphase accelerator to use (not null)
+     */
+    public PhysicsSoftSpace(BroadphaseType broadphaseType) {
+        this(new Vector3f(-10000f, -10000f, -10000f),
+                new Vector3f(10000f, 10000f, 10000f), broadphaseType);
+    }
+
+    /**
+     * Instantiate a PhysicsSoftSpace with a sequential-impulse solver. Must be
+     * invoked on the designated physics thread.
+     *
      * @param worldMin the desired minimum coordinate value for each axis (not
      * null, unaffected, default=(-10k,-10k,-10k))
      * @param worldMax the desired maximum coordinate value for each axis (not
