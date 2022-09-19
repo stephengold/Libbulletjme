c6cd2244e9f5b1afa1c26d881e5dd425819b65a8
Stephen Gold <sgold@sonic.net>
2022-09-19 15:49:00 -0700

    bugfix:  missing imports in CompoundMesh

--- src/main/java/com/jme3/bullet/collision/shapes/infos/CompoundMesh.java
+++ src/main/java/com/jme3/bullet/collision/shapes/infos/CompoundMesh.java
@@ -33,12 +33,16 @@ package com.jme3.bullet.collision.shapes.infos;
 
 import com.jme3.bullet.NativePhysicsObject;
 import com.jme3.math.Plane;
+import com.jme3.math.Transform;
 import com.jme3.math.Triangle;
 import com.jme3.math.Vector3f;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jme3utilities.Validate;
+import jme3utilities.math.MyBuffer;
 import jme3utilities.math.MyVector3f;
 
 /**
