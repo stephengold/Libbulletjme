3bb4b235102829ccf75ad603f2fbee4e788f570a
Stephen Gold <sgold@sonic.net>
2022-09-23 15:12:37 -0700

    CollisionSpace:  delete unnecessary validation

--- src/main/java/com/jme3/bullet/CollisionSpace.java
+++ src/main/java/com/jme3/bullet/CollisionSpace.java
@@ -355,9 +355,6 @@ public class CollisionSpace extends NativePhysicsObject {
      * @return true if registered, otherwise false
      */
     public boolean hasClosest(CollisionShape shape0, CollisionShape shape1) {
-        Validate.nonNull(shape0, "shape 0");
-        Validate.nonNull(shape1, "shape 1");
-
         long spaceId = nativeId();
         int shape0Type = shape0.getShapeType();
         int shape1Type = shape1.getShapeType();
@@ -375,9 +372,6 @@ public class CollisionSpace extends NativePhysicsObject {
      * @return true if registered, otherwise false
      */
     public boolean hasContact(CollisionShape shape0, CollisionShape shape1) {
-        Validate.nonNull(shape0, "shape 0");
-        Validate.nonNull(shape1, "shape 1");
-
         long spaceId = nativeId();
         int shape0Type = shape0.getShapeType();
         int shape1Type = shape1.getShapeType();
