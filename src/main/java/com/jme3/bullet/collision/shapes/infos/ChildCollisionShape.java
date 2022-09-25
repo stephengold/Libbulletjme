811c0c3c24e78878eb4a85b415c01967f28d1cd3
Stephen Gold <sgold@sonic.net>
2022-09-24 23:35:47 -0700

    bugfix:  shape's scale is ignored by ChildCollisionShape.split()

--- src/main/java/com/jme3/bullet/collision/shapes/infos/ChildCollisionShape.java
+++ src/main/java/com/jme3/bullet/collision/shapes/infos/ChildCollisionShape.java
@@ -230,6 +230,7 @@ public class ChildCollisionShape {
         HullCollisionShape hull = (HullCollisionShape) shape; // TODO more cases
 
         Transform c2pTransform = copyTransform(null);
+        shape.getScale(c2pTransform.getScale()); // Copy the scale factors.
         Triangle childTriangle
                 = MyMath.transformInverse(c2pTransform, parentTriangle, null);
         ChildCollisionShape[] mp = hull.split(childTriangle);
