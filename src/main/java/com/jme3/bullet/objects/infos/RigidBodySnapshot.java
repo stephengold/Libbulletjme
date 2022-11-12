3737bf3b9851b4fb26ea2025113000560ac11b70
Stephen Gold <sgold@sonic.net>
2022-11-11 18:31:14 -0800

    RigidBodySnapshot:  rename 2 fields due to typos

--- src/main/java/com/jme3/bullet/objects/infos/RigidBodySnapshot.java
+++ src/main/java/com/jme3/bullet/objects/infos/RigidBodySnapshot.java
@@ -131,7 +131,7 @@ public class RigidBodySnapshot {
     /**
      * anisotropic friction modes (bitmask)
      */
-    final protected int anisotopicFrictionModes; // TODO rename
+    final protected int anisotropicFrictionModes;
     /**
      * native IDs of all collision objects in the ignore list
      */
@@ -159,7 +159,7 @@ public class RigidBodySnapshot {
     /**
      * anisotropic friction components
      */
-    final protected Vector3f anisotopicFrictionComponents; // TODO rename
+    final protected Vector3f anisotropicFrictionComponents;
     /**
      * linear factors
      */
@@ -208,7 +208,7 @@ public class RigidBodySnapshot {
                 afMode |= bitMask;
             }
         }
-        this.anisotopicFrictionModes = afMode;
+        this.anisotropicFrictionModes = afMode;
 
         this.ignoreList = body.listIgnoredIds();
         this.rotationMatrix = body.getPhysicsRotationMatrix(null);
@@ -224,7 +224,7 @@ public class RigidBodySnapshot {
         this.location = body.getPhysicsLocationDp(null);
 
         // Vector3f
-        this.anisotopicFrictionComponents = body.getAnisotropicFriction(null);
+        this.anisotropicFrictionComponents = body.getAnisotropicFriction(null);
         this.angularFactor = body.getAngularFactor(null);
         this.linearFactor = body.getLinearFactor(null);
         this.totalAppliedForce = body.totalAppliedForce(null);
@@ -260,7 +260,7 @@ public class RigidBodySnapshot {
         body.setSpinningFriction(spinningFriction);
 
         body.setAnisotropicFriction(
-                anisotopicFrictionComponents, anisotopicFrictionModes);
+                anisotropicFrictionComponents, anisotropicFrictionModes);
         body.setIgnoreList(ignoreList);
         body.setPhysicsRotation(rotationMatrix);
         body.setPhysicsLocationDp(location);
