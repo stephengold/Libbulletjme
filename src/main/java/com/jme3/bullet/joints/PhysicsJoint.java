ba530dd883120a25b185ea96067b344de82337be
Stephen Gold <sgold@sonic.net>
2022-12-31 20:08:54 -0800

    bugfix:  assert fails when toString() is invoked on unassigned joint

--- src/main/java/com/jme3/bullet/joints/PhysicsJoint.java
+++ src/main/java/com/jme3/bullet/joints/PhysicsJoint.java
@@ -260,8 +260,12 @@ abstract public class PhysicsJoint extends NativePhysicsObject {
         result = result.replace("Physics", "");
         result = result.replace("Point", "P");
         result = result.replace("Six", "6");
-        long jointId = nativeId();
-        result += "#" + Long.toHexString(jointId);
+        if (hasAssignedNativeObject()) {
+            long jointId = nativeId();
+            result += "#" + Long.toHexString(jointId);
+        } else {
+            result += "#unassigned";
+        }
 
         return result;
     }
