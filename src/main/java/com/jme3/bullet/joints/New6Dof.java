a1a0090f017167290361007fb8b9fb46f257c3da
Stephen Gold <sgold@sonic.net>
2023-01-23 11:55:24 -0800

    New6Dof:  add the getRotationMatrix() method

--- src/main/java/com/jme3/bullet/joints/New6Dof.java
+++ src/main/java/com/jme3/bullet/joints/New6Dof.java
@@ -368,6 +368,31 @@ public class New6Dof extends Constraint {
     }
 
     /**
+     * Copy the the orientation of the constraint in the specified end's
+     * coordinate system.
+     *
+     * @see com.jme3.bullet.joints.Constraint#getPivot(
+     * com.jme3.bullet.joints.JointEnd, com.jme3.math.Vector3f)
+     *
+     * @param end which end (not null)
+     * @param storeResult storage for the result (modified if not null)
+     * @return a rotation matrix (either storeResult or a new instance)
+     */
+    public Matrix3f getRotationMatrix(JointEnd end, Matrix3f storeResult) {
+        Validate.nonNull(end, "end");
+        Matrix3f result = (storeResult == null) ? new Matrix3f() : storeResult;
+
+        if (end == JointEnd.A) {
+            result.set(rotA);
+        } else {
+            assert end == JointEnd.B : end;
+            result.set(rotB);
+        }
+
+        return result;
+    }
+
+    /**
      * Access the indexed RotationMotor of this constraint, the motor which
      * influences rotation around the indexed axis.
      *
