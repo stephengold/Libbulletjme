6729ec0971818c993185bdbc4e581a197ebf48c2
stephengold <sgold@sonic.net>
2024-04-24 14:11:53 -0700

    btQuantizedBvh.cpp:  verify the size of btQuantizedBvh

--- src/main/native/bullet3/BulletCollision/BroadphaseCollision/btQuantizedBvh.cpp
+++ src/main/native/bullet3/BulletCollision/BroadphaseCollision/btQuantizedBvh.cpp
@@ -36,6 +36,7 @@ btQuantizedBvh::btQuantizedBvh() : m_bulletVersion(BT_BULLET_VERSION),
         btAssert(sizeof(btQuantizedBvhNode) == 16); // stephengold added 2024-04-19
         btAssert(sizeof(btOptimizedBvhNode) % 32 == 0); // stephengold added 2024-04-19
         btAssert(sizeof(btBvhSubtreeInfo) == 32); // stephengold added 2024-04-19
+        btAssert(sizeof(btQuantizedBvh) == 248); // stephengold added 2024-04-24
 	m_bvhAabbMin.setValue(-SIMD_INFINITY, -SIMD_INFINITY, -SIMD_INFINITY);
 	m_bvhAabbMax.setValue(SIMD_INFINITY, SIMD_INFINITY, SIMD_INFINITY);
 }
