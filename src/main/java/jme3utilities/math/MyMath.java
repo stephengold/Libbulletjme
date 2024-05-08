34c289bd3112258b5e264dbb8721cfc020e6b069
stephengold <sgold@sonic.net>
2024-05-08 16:13:22 -0700

    MyMath:  javadoc

--- src/main/java/jme3utilities/math/MyMath.java
+++ src/main/java/jme3utilities/math/MyMath.java
@@ -74,7 +74,7 @@ final public class MyMath {
     // new methods exposed
 
     /**
-     * Return the area of the specified triangle.
+     * Return the (one-sided) area of the specified triangle.
      *
      * @param triangle (not null, unaffected)
      * @return the area (&ge;0)
